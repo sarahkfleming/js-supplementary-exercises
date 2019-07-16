bddcbacb1b20008c876275937167a83767363e6d
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-16 12:12:31 -0500

    refactored cash-to-coins code

--- cash-to-coins/cashToCoins.js
+++ cash-to-coins/cashToCoins.js
@@ -1,4 +1,4 @@
-const dollarAmount = 4.05
+const dollarAmount = 4.87
 const piggyBank = {}
 
 piggyBank.quarters = 0
@@ -6,16 +6,20 @@ piggyBank.dimes = 0
 piggyBank.nickels = 0
 piggyBank.pennies = 0
 
-const convertDollars = dollarAmount * 100
-let remainingCash = 0
+const coinValues = {
+    quarters: 25,
+    dimes: 10,
+    nickels: 5,
+    pennies: 1
+}
 
-piggyBank.quarters = Math.floor(convertDollars/25)
-remainingCash = convertDollars - (piggyBank.quarters * 25)
-piggyBank.dimes = Math.floor(remainingCash/10)
-remainingCash -= piggyBank.dimes * 10
-piggyBank.nickels = Math.floor(remainingCash/5)
-remainingCash -= piggyBank.nickels * 5
-piggyBank.pennies = Math.floor(remainingCash/1)
-remainingCash -= piggyBank.pennies * 1
+const convertDollars = dollarAmount * 100
+let remainingCash = convertDollars
+
+for (const coinType in coinValues) {
+    const coinWorth = coinValues[coinType];
+    piggyBank[coinType] = Math.floor(remainingCash/coinWorth)
+    remainingCash -= piggyBank[coinType] * coinWorth
+}
 
 console.log(piggyBank)
