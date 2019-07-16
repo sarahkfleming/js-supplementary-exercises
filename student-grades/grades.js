2b5ca848220e555f7b47a550a309020b366e1eb5
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-16 14:15:52 -0500

    Finished part 2 of student grades exercise

--- student-grades/grades.js
+++ student-grades/grades.js
@@ -11,28 +11,28 @@ Start with array of random scores in your JavaScript
 
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
 const grades = {
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0,
-    F: 0,
+  A: 0,
+  B: 0,
+  C: 0,
+  D: 0,
+  F: 0,
 }
 
 for (let i = 0; i < scores.length; i++) {
-    /*
-      If the score is greater than 90, increment grades.A by 1.
+  /*
+    If the score is greater than 90, increment grades.A by 1.
 
-      You can use a series of `if/then` blocks, but you could
-      also achieve this with a switch statement.
-    */
-switch(true) {
-    case scores[i] > 90 :
+    You can use a series of `if/then` blocks, but you could
+    also achieve this with a switch statement.
+  */
+  switch (true) {
+    case scores[i] > 90:
       grades.A += 1
       break;
     case scores[i] > 80:
       grades.B += 1
       break;
-      case scores[i] > 70 :
+    case scores[i] > 70:
       grades.C += 1
       break;
     case scores[i] > 60:
@@ -42,16 +42,42 @@ switch(true) {
       grades.F += 1
       break;
     default:
-        console.log("If this shows, there's an error")
+      console.log("If this shows, there's an error")
   }
 }
 
 //Use console.info() to output the following criteria to the browser console.
+
 // How many of each grade? Accomplish this with a for..in loop.
+for (const letterGrade in grades) {
+  const gradeNumbers = grades[letterGrade];
+}
+console.info(grades)
 
 // What is the lowest score? Sort the array and find out.
+scores.sort()
+console.info(scores[0])
 
 // What is the highest score?
+scores.reverse()
+console.info(scores[0])
 
 // Which grade had the most students achieve it? Use an if statment, and a currentGradeCount variable, in your for..in loop to see if the current grade's value is higher than the last one.
+
+
+
 // Which grade had the fewest students achieve it?
+
+/*
+NOTES
+----------------------------------------------------------------
+Numeric Sort
+By default, the sort() function sorts values as strings.
+This works well for strings ("Apple" comes before "Banana").
+However, if numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+Because of this, the sort() method will produce incorrect result when sorting numbers.
+You can fix this by providing a compare function:
+
+var points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+*/
